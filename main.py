0cecf2f655d75825f72a3dd078fc4b1376363143
Jacqui <JSangster1998@gmail.com>
2019-10-15 20:19:10 +0100

    danger noodles r us

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
 # your contributions here
+import time
+
+i = 3
+snek1 ="""
+                         ____
+ _______________________/ O  \___/
+<____________________________/   \ 
+"""
+
+snek2 ="""
+    ____
+\__/o   \____________________
+/  \_________________________>
+"""
+
+for j in range(i):
+    if j % 2 == 0:
+        print(snek1)
+    else:
+        print(snek2)
+    time.sleep(1)
+
+       
